db6edd42ab93099a7250470b06b07292aade2e48
Shaoor Ud Din Hashmi <shaoor@webisoft.com>
2022-11-06 15:04:44 -0500

    new update

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,13 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from "./components/NavBar";
 import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
+import {ProductProvider} from "./ProductContext";
 
 function App() {
     return (
         <div>
             <Router>
                 <Switch>
-                    <NavBar/>
+                    <ProductProvider>
+                        <NavBar/>
+                    </ProductProvider>
                 </Switch>
             </Router>
         </div>
--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -1,17 +1,19 @@
-import react from "react"
+import react, {useContext} from "react"
 import {Navbar, Nav, Form, FormControl, Button, Badge} from "react-bootstrap";
 import {Link} from "react-router-dom";
+import {ProductContext} from "../ProductContext";
 
 const NavBar = () => {
+    const [products, setProducts] = useContext(ProductContext)
     return (
         <Navbar bg="dark" expand="lg" variant="dark">
             <Navbar.Brand href="#home">Inventory Management App</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Badge className="mt-2" variant="primary">Products In stock </Badge>
+                    <Badge className="mt-2" variant="primary">Products In stock {products.data.length}</Badge>
                 </Nav>
-                <Form  inline>
+                <Form inline>
                     <Link to="/addproduct" className="btn btn-primary btn-sm mr-4">Add Product</Link>
                     <FormControl type="text" placeholder="Search"
                                  className="mr-sm-2"/>
