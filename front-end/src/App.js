6aaaaa822ad575f0a11326c7e0ffeb6d925ccdc5
Shaoor Ud Din Hashmi <shaoor@webisoft.com>
2022-11-15 10:46:53 -0500

    update  product

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,25 +5,32 @@ import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
 import {ProductProvider} from "./ProductContext";
 import ProductsTable from "./components/ProductTable";
 import AddProducts from "./components/AddProducts";
-
+import UpdateProduct from "./components/UpdateProduct";
+import {UpdateProductContextProvider} from "./UpdateProductContext";
+import {SupplierContextProvider} from "./SupplierContext";
 function App() {
-    return (
-        <div>
-            <Router>
-                <Switch>
-                    <ProductProvider>
-                        <NavBar/>
-                        <div className="row">
-                            <div className="col-sm-10 col-xm-12 mr-auto mr-auto ml-auto mt-4 mb-4">
-                                <Route exact path='/' component={ProductsTable}/>
-                                <Route exact path="/addproduct" component={AddProducts}/>
-                            </div>
-                        </div>
-                    </ProductProvider>
-                </Switch>
-            </Router>
-        </div>
-    );
-}
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <ProductProvider>
+            <NavBar />
+            <div className="row">
+              <div className="col-sm-10 col-xm-12 mr-auto ml-auto mt-4 mb-4">
+                <UpdateProductContextProvider>
+                <SupplierContextProvider>
+                    <Route exact path='/' component={ProductsTable} />
+                    <Route exact path='/updateproduct' component={UpdateProduct} />
+                    <Route exact path="/addproduct" component={AddProducts} />
+                </SupplierContextProvider>
+                </UpdateProductContextProvider>
 
+              </div>
+            </div>
+          </ProductProvider>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
 export default App;
